05412c2f2b8832a2130229f6ccc78cf63991afaf
ESaracay <evansito14@gmail.com>
2024-05-14 15:16:51 -0700

    Created anew next.config.js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/SoftwareGuide",
-    output: "export",
-    reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
