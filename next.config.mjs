5534ed00599fb64bca9647276df9dd4794393dce
ESaracay <evansito14@gmail.com>
2024-05-14 15:03:13 -0700

    Added workflow for launching application to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/SoftwareGuide",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
